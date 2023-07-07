0a7d721ee36ee4cfcc9b681122b1ad4963661e94
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2023-07-07 14:59:57 -0300

    SocialMedia fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Carousel from "./components/carousel";
+import SocialMedia from "./components/socialMedia";
 import Navbar from "./components/navbar";
 import { Instagram, Youtube, Phone } from "react-feather";
 import Background from "./assets/images/camera.jpg";
@@ -16,7 +17,7 @@ function App() {
   return (
     <>
       <img
-        className="fixed top-5 left-5 h-20 w-20 rounded-full z-50"
+        className="fixed top-5 left-5 h-20 w-20 rounded-full z-50 shadow-custom"
         src={Logo}
         alt="Logo"
         title="Logo"
@@ -30,30 +31,14 @@ function App() {
             alt="Camera and desktop"
             title="Camera and desktop"
           />
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 
-                        -translate-y-1/2 text-white sm:text-4xl md:text-4xl
-                        lg:text-5xl xl:text-7xl font-bold text-center">
-            NDI PRODUCTORA
-          </div>
-      </div>
-        <div className="bg-transparent flex flex-row h-18 p-2 mt-[-72px]">
-          <a href="https://www.instagram.com/ndiproductora?hl=es-la" target="_blank">
-            <Instagram className="stroke-1 text-yellow-500 h-auto w-14 
-                                  mx-6 transition-transform duration-300 
-                                  transform-gpu hover:-translate-y-5"/>
-          </a>
-
-          <a href="https://www.youtube.com/@ndiproductora7060/" target="_blank">
-            <Youtube className="stroke-1 text-yellow-500 h-auto w-14
-                                mr-6 transition-transform duration-300
-                                transform-gpu hover:-translate-y-5" />
-          </a>
-
-          <a href="#" target="_blank">
-            <Phone className="stroke-1 text-yellow-500 h-auto w-14
-                              mr-6 transition-transform duration-300
-                              transform-gpu animate-pulse-transform" />
-          </a>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 
+                          -translate-y-1/2 text-white sm:text-4xl md:text-4xl
+                          lg:text-5xl xl:text-7xl font-bold text-center">
+              NDI PRODUCTORA
+            </div>
+        </div>
+        <div className="relative top-[-110px]">
+          <SocialMedia/>
         </div>
       </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,8 @@ module.exports = {
         'orange': '#FCA311',
         'white': '#fff',
       },
-    },
-    animation: {
-      'pulse-transform': 'pulseTransform 1.5s ease-in-out infinite',
-    },
-    keyframes: {
-      pulseTransform: {
-        '0%': { transform: 'scale(1)' },
-        '50%': { transform: 'scale(1.1)' },
-        '100%': { transform: 'scale(1)' },
+      boxShadow: {
+        'custom': '6px 5px 6px #2E4163, -6px -5px 6px #FCA311',
       },
     },
   },
