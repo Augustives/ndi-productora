13c1b750eceda11c6199067345d16abdb8d7fbc8
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2023-07-07 10:25:04 -0300

    arrumei a navbar, os logos e o texto ao centro do background.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'darkBlue': '#14213D',
+        'softBlue': '#2E4163',
+        'orange': '#FCA311',
+        'white': '#fff',
+      },
+    },
+    animation: {
+      'pulse-transform': 'pulseTransform 1.5s ease-in-out infinite',
+    },
+    keyframes: {
+      pulseTransform: {
+        '0%': { transform: 'scale(1)' },
+        '50%': { transform: 'scale(1.1)' },
+        '100%': { transform: 'scale(1)' },
+      },
+    },
   },
+  variants: {},
   plugins: [],
 };
