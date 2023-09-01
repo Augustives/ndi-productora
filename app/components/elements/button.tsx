e93f7abfd94060f77e1c41e1ef419acd871c74fd
Augustives <augusto1408@hotmail.com>
2023-09-01 15:04:20 -0300

    Remove underline

--- app/components/elements/button.tsx
+++ app/components/elements/button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ text, href }) => {
         href={href}
         className="
           inline-flex h-8 items-center justify-center hover:-translate-y-2 duration-500
-          px-10 text-center align-middle font-custom text-xl text-white hover:underline hover:underline-offset-4
+          px-10 text-center align-middle font-custom text-xl text-white 
         "
       >
         {text}
